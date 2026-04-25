07bc719827fee75d759cc6148ec22e91013376f1
paletochen <davidcastro@google.com>
2026-04-25 03:14:56 +0200

    v3

--- shell.js
+++ shell.js
@@ -6,7 +6,7 @@
 
 // Single source of truth for the web release. assemble-web.sh stamps
 // this into dist/sw.js (VERSION) and dist/index.html (softwareVersion).
-const WEB_VERSION = '4.11v2';
+const WEB_VERSION = '4.11v3';
 
 
 
